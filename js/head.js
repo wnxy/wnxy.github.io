ddf1b4ff2c697f999a5f016168d9f4b506fb6de6
wnxy <3118824295@qq.com>
2020-02-24 22:29:24 +0800

    Site updated: 2020-02-24 22:29:15

---
+++ js/head.js
@@ -0,0 +1,42 @@
+$(function () {
+  $('.toggle-menu').on('click', function () {
+    if (!$('.menus').is(':visible')) {
+      $('.menus').velocity('stop')
+        .velocity('transition.slideDownIn', { duration: 300 })
+    } else {
+      $('.menus').velocity('stop')
+        .velocity('transition.slideUpOut', { duration: 300 })
+    }
+  })
+  $(document).on('click', function (e) {
+    var flag = $('.menus')[0].contains(e.target) || $('.toggle-menu')[0].contains(e.target)
+    if (!flag && $('.toggle-menu').is(':visible')) {
+      $('.menus').velocity('stop')
+        .velocity('transition.slideUpOut', { duration: 300 })
+    }
+  })
+  $(window).on('resize', function (e) {
+    if (!$('.toggle-menu').is(':visible')) {
+      if (!$('.menus').is(':visible')) {
+        $('.menus').velocity('stop')
+          .velocity('transition.slideDownIn', { duration: 300 })
+      }
+    }
+  })
+})
+
+<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
+
