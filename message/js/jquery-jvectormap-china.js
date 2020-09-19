0fe9008f7340add14732e234a60f808773060f7f
wnxy <3118824295@qq.com>
2020-09-19 11:32:16 +0800

    Site updated: 2020-09-19 11:32:00

--- message/js/jquery-jvectormap-china.js
+++ message/js/jquery-jvectormap-china.js
@@ -1,4 +1,4 @@
-jQuery.fn.vectorMap('addMap', 'china', {
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>jQuery.fn.vectorMap('addMap', 'china', {
 	"insets": [{
 		"width": 900,
 		"top": 0,
